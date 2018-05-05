ee218195ad9f6a15d998638c6ffe351fae271fde
YuDing Sun <yudingsun@gmail.com>
2018-05-05 12:03:52 -0700

    Daily update

--- src/LoginNumbersPerInterval.java
+++ src/LoginNumbersPerInterval.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class LoginNumbersPerInterval {
+    public int[][] sessionNum(int[][] logins) {
+        Arrays.sort(logins, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        return null;
+    }
 }
