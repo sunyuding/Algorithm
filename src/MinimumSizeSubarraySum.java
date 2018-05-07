5ebe73c2536aad91386cac97c43dab74b7f2f781
YuDing Sun <yudingsun@gmail.com>
2018-05-06 22:26:17 -0700

    Daily Update

---
+++ src/MinimumSizeSubarraySum.java
@@ -0,0 +1,37 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * Given an array of n positive integers and a positive integer s,
+ * find the minimal length of a contiguous subarray of which the sum ≥ s.
+ * If there isn't one, return 0 instead.
+
+ For example,
+ given the array [2,3,1,2,4,3] and s = 7,
+ the subarray [4,3] has the minimal length under the problem constraint.
+
+ Medium
+ Array
+ Two Pointers
+ Binary Tree
+ */
+public class MinimumSizeSubarraySum {
+    public int minSubArrayLen(int s, int[] nums) {
+        for (int len = 1; len <= nums.length; len++) {
+            for (int i = 0; i <= nums.length - len; i++) {
+                if (getSum(nums, i, i + len) >= s) {
+                    return len;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private int getSum(int[] nums, int start, int end) {
+        int result = 0;
+        for (int i = start; i < end; i++) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
---
+++ src/ThreeSum.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Determine if there exists three elements in a given array that sum to the given target number.
+ * Return all the triple of values that sums to target.
+
+ Assumptions
+
+ The given array is not null and has length of at least 3
+ No duplicate triples should be returned, order of the values in the tuple does not matter
+ Examples
+
+ A = {1, 2, 2, 3, 2, 4}, target = 8, return [[1, 3, 4], [2, 2, 4]]
+
+ Medium
+ Array
+ Sort
+ */
+public class ThreeSum {
+    public List<List<Integer>> allTriples(int[] array, int target) {
+        Arrays.sort(array);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < array.length - 2; i++) {
+            if (i == 0 || array[i] != array[i - 1]) {
+                int j = i + 1;
+                int k = array.length - 1;
+                while (j < k) {
+                    // array[i] <= array[j] <= array[k]
+                    int sum = array[i] + array[j] + array[k];
+                    if (sum == target) {
+                        List<Integer> cur = new ArrayList<>();
+                        cur.add(array[i]);
+                        cur.add(array[j]);
+                        cur.add(array[k]);
+                        result.add(cur);
+                        j++;
+                        k--;
+                        while (j < k && array[j] == array[j - 1]) {
+                            j++;
+                        }
+                        while (j < k && array[k] == array[k + 1]) {
+                            k--;
+                        }
+                    } else if (sum < target) {
+                        j++;
+                    } else {
+                        k--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
