318ae517c43d527b4289d9ebd93afb242207352c
sunyuding <yudingsun@gmail.com>
2019-04-25 15:46:17 -0700

    Thursday daytime update

---
+++ src/Tree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,23 @@
+package Tree;
+
+import java.util.*;
+
+public class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        if (root == null) return result;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.add(root);
+        while(!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            result.addFirst(cur.val);
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+        }
+        return result;
+    }
+}
