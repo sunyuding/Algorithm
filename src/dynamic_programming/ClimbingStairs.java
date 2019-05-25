066f79d9fb633a019a8f17b59c70c095f7ebb3b2
sunyuding <yudingsun@gmail.com>
2019-05-24 17:55:49 -0700

    ClimbingStairs EditDistance MinimumPathSum

---
+++ src/dynamic_programming/ClimbingStairs.java
@@ -0,0 +1,60 @@
+package dynamic_programming;
+//You are climbing a stair case.
+// It takes n steps to reach to the top.
+//
+//        Each time you can either climb 1 or 2 steps.
+// In how many distinct ways can you climb to the top?
+//
+//        Note: Given n will be a positive integer.
+//
+//        Example 1:
+//
+//        Input: 2
+//        Output: 2
+//        Explanation: There are two ways to climb to the top.
+//        1. 1 step + 1 step
+//        2. 2 steps
+//        Example 2:
+//
+//        Input: 3
+//        Output: 3
+//        Explanation: There are three ways to climb to the top.
+//        1. 1 step + 1 step + 1 step
+//        2. 1 step + 2 steps
+//        3. 2 steps + 1 step
+//public class ClimbingStairs {
+//    /**
+//     * Time: O(n)
+//     * Space: O(n)
+//     * @param n
+//     * @return
+//     */
+//    public int climbStairs(int n) {
+//        int[] dp = new int[n + 1];
+//        dp[0] = 1;
+//        dp[1] = 1;
+//        for (int i = 2; i <= n; i++) {
+//            dp[i] = dp[i - 1] + dp[i - 2];
+//        }
+//        return dp[n];
+//    }
+//}
+
+public class ClimbingStairs {
+    /**
+     * Time: O(n)
+     * Space: O(1)
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        int oneStep = 1;
+        int twoStep = 1;
+        for (int i = 2; i <= n; i++) {
+            int newOneStep = oneStep + twoStep;
+            twoStep = oneStep;
+            oneStep = newOneStep;
+        }
+        return oneStep;
+    }
+}
